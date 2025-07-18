ac9614bf73b25fcb0e2cb76d022a14396443236a
Rischh <rischebourg.samuel@gmail.com>
2025-07-18 18:33:28 +0200

    feat: add the basic layout of the main view

--- MultiplicationTrainer/ContentView.swift
+++ MultiplicationTrainer/ContentView.swift
@@ -7,15 +7,43 @@
 
 import SwiftUI
 
+struct Question {
+    let text: String
+    let anwser: Int
+}
+
 struct ContentView: View {
+    @State private var selectedTable = 2
+    @State private var selectedQuestionCount = 5
+    @State private var questions = [Question]()
+    
+    let questionCount = [5, 10, 20]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Stepper("Table of : \(selectedTable)", value: $selectedTable, in: 2...12)
+        
+        Picker("Question count", selection: $selectedQuestionCount) {
+            ForEach(questionCount, id: \.self) { number in
+               Text("\(number)")
+            }
         }
-        .padding()
+        .pickerStyle(.segmented)
+        
+        Button("Start the game") {
+            generateQuestions(selectedTable, selectedQuestionCount)
+            // ouvre la nouvelle Page/Vue
+        }
+    }
+    
+    func generateQuestions(_ table: Int, _ questionCount: Int) {
+        for _ in 0..<questionCount {
+            let randomNumber = Int.random(in: 2...10) // à faire: Générer un nombre aléatoire qui n'est pas déja apparu
+            let question = Question(text: "\(table) * \(randomNumber)", anwser: table * randomNumber)
+            
+            questions.append(question)
+        }
+        
+        print(questions)
     }
 }
 
