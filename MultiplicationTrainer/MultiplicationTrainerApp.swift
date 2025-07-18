4f3b51d7a095f71a219ed41bf6faff45d296fc10
Rischh <rischebourg.samuel@gmail.com>
2025-07-18 17:45:07 +0200

    Initial Commit

---
+++ MultiplicationTrainer/MultiplicationTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTrainerApp.swift
+//  MultiplicationTrainer
+//
+//  Created by Samuel Rischebourg on 18/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
