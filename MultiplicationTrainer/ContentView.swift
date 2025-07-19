5f5f40de306b3d9439ab8454a8800d809998734a
Rischh <rischebourg.samuel@gmail.com>
2025-07-19 17:47:25 +0200

    feat: add a very basic UI on ContentView

--- MultiplicationTrainer/ContentView.swift
+++ MultiplicationTrainer/ContentView.swift
@@ -7,43 +7,74 @@
 
 import SwiftUI
 
-struct Question {
+struct QuestionItem: Identifiable {
+    let id = UUID()
     let text: String
     let anwser: Int
+}
+
+@Observable
+class Questions {
+    var items = [QuestionItem]()
 }
 
 struct ContentView: View {
     @State private var selectedTable = 2
     @State private var selectedQuestionCount = 5
-    @State private var questions = [Question]()
+    @State private var questions = Questions()
+    
+    @State private var showingSheet = false
     
     let questionCount = [5, 10, 20]
     
     var body: some View {
-        Stepper("Table of : \(selectedTable)", value: $selectedTable, in: 2...12)
-        
-        Picker("Question count", selection: $selectedQuestionCount) {
-            ForEach(questionCount, id: \.self) { number in
-               Text("\(number)")
+        NavigationStack {
+            Divider()
+            
+            ZStack {
+                Image("Blue Squared Sheet").resizable()
+                
+                VStack {
+                    Spacer()
+                    
+                    VStack {
+                        Stepper("Table of : \(selectedTable)", value: $selectedTable, in: 2...12)
+                        
+                        Picker("Question count", selection: $selectedQuestionCount) {
+                            ForEach(questionCount, id: \.self) { number in
+                                Text("\(number)")
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    .padding()
+                    
+                    Spacer()
+                    
+                    
+
+                }
+                .navigationTitle("Multiplication Trainer")
             }
-        }
-        .pickerStyle(.segmented)
-        
-        Button("Start the game") {
-            generateQuestions(selectedTable, selectedQuestionCount)
-            // ouvre la nouvelle Page/Vue
+            Divider()
+            
+            Button("Start the game") {
+                generateQuestions(selectedTable, selectedQuestionCount)
+                showingSheet = true
+            }
+            .sheet(isPresented: $showingSheet) {
+                TestView(questions: questions)
+            }
         }
     }
     
     func generateQuestions(_ table: Int, _ questionCount: Int) {
         for _ in 0..<questionCount {
             let randomNumber = Int.random(in: 2...10) // à faire: Générer un nombre aléatoire qui n'est pas déja apparu
-            let question = Question(text: "\(table) * \(randomNumber)", anwser: table * randomNumber)
             
-            questions.append(question)
+            let question = QuestionItem(text: "\(table) * \(randomNumber)", anwser: table * randomNumber)
+            questions.items.append(question)
         }
-        
-        print(questions)
     }
 }
 
